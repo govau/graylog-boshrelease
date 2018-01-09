2865d1d5a185494b65089a9979aedcbeedbc1da7
Michael Richardson <michael.richardson@digital.gov.au>
2018-01-09 15:47:41 +1100

    correct pipeline name to be graylog. update pipeline resources now that repo has been migrated from `AusDTO` Github org to `govau`

--- ci/create-pipeline.sh
+++ ci/create-pipeline.sh
@@ -7,6 +7,6 @@ TARGET=${TARGET:-local}
 
 fly validate-pipeline --config pipeline.yml
 
-fly --target ${TARGET} set-pipeline --config pipeline.yml --pipeline create-boshrelease-instant-https -n -l credentials.yml
+fly --target ${TARGET} set-pipeline --config pipeline.yml --pipeline create-boshrelease-graylog -n -l credentials.yml
 
-fly -t ${TARGET} unpause-pipeline -p create-boshrelease-instant-https
+fly -t ${TARGET} unpause-pipeline -p create-boshrelease-graylog
